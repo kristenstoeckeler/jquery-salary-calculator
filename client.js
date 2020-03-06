fcd740d424d82561dafc67a02ff6bbdad9c37adf
Kristen Stoeckeler <kstoeckeler@gmail.com>
2020-03-06 16:06:40 -0600

    created files, added basic html buildout, setup JS doc with readyNow

---
+++ client.js
@@ -0,0 +1,12 @@
+console.log( 'Salary Calculator' );
+
+$( document ).ready( readyNow );
+
+function readyNow(){
+    console.log( 'JQ' );
+    $('#btn-submit').on('click' calculateSalary);    
+}
+
+function calculateSalary(){
+    
+}
