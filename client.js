f15e671147ecd280b33ef4b272ce24bc0826d7f7
Kristen Stoeckeler <kstoeckeler@gmail.com>
2020-03-06 16:27:21 -0600

    created employeeObject function

--- client.js
+++ client.js
@@ -2,11 +2,40 @@ console.log( 'Salary Calculator' );
 
 $( document ).ready( readyNow );
 
+employees = [];
+
 function readyNow(){
     console.log( 'JQ' );
-    $('#btn-submit').on('click' calculateSalary);    
+    $('#btn-submit').on('click', employeeObject );    
 }
 
-function calculateSalary(){
+function employeeObject(){
+    console.log( 'in employeeObject' );
+    let employee = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        idNumber: $('#idNumber').val(),
+        jobTitle: $('#jobTitle').val(),
+        annualSalary: $('#annualSalary').val()
+    }
+    console.log( employee );
+    employees.push( employee );
+    console.log( employees );
+    calculateMonthly();
+}
+
+function calculateMonthly(){
+    console.log( 'in calculateMonthly' );
     
+
+
+    appendDOM();
+}
+
+function appendDOM(){
+    console.log( 'in appendDOM' );
+
+
+
+
 }
