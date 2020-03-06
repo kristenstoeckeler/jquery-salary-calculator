a72d03d4d770e8e2fd12a136ec8c15f7acb9da03
Kristen Stoeckeler <kstoeckeler@gmail.com>
2020-03-06 16:52:58 -0600

    calculateMonthly function

--- client.js
+++ client.js
@@ -26,16 +26,22 @@ function employeeObject(){
 
 function calculateMonthly(){
     console.log( 'in calculateMonthly' );
-    
-
-
-    appendDOM();
+    let annualCost = 0;
+    let monthlyCost = 0;
+    for (employee of employees){
+        console.log( 'in for loop' );
+        annualCost = annualCost + Number(employee.annualSalary);
+    }
+    monthlyCost = annualCost / 12;
+    console.log( monthlyCost );
+    // appendDOM();
 }
 
-function appendDOM(){
-    console.log( 'in appendDOM' );
-
-
-
-
-}
+// function appendDOM(){
+//     console.log( 'in appendDOM' );
+//     let $el1= 
+//     let $el2 =
+//     let $el3 =
+//     let $el4 =
+//     let $el5 =
+// }
